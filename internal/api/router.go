602e3a236ba0ba23abf0b2bffa29940789341567
Johnson Osei Yeboah <johnyoat@gmail.com>
2026-02-20 17:14:24 -0600

    feat(api): added health and rount not found endpoint

--- internal/api/router.go
+++ internal/api/router.go
@@ -7,7 +7,32 @@ import (
 )
 
 func InitRouter(app *fiber.App, queries *db.Queries) {
+
+	//Root Endpoint
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"service": "kanshi-core",
+			"status":  "running",
+		})
+	})
+
+	//Health Endpoint
+	app.Get("/health", func(c fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status": "ok",
+		})
+	})
+
+	//Versioning Init
 	api := app.Group("/api")
 	v1Group := api.Group("/v1")
 	v1.Init(v1Group, queries)
+
+	//404 Endpoint
+	app.Use(func(c fiber.Ctx) error {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "route not found",
+			"path":  c.Path(),
+		})
+	})
 }
