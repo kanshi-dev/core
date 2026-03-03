e28df4258c7c3618d43c3ec356c4984a56ed75b2
Johnson Osei Yeboah <johnyoat@gmail.com>
2026-03-02 20:43:22 -0600

    chore: added cors()

--- internal/api/server.go
+++ internal/api/server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/kanshi-dev/core/internal/service"
 )
 
@@ -13,6 +14,7 @@ type Server struct {
 
 func NewServer(agentService *service.AgentsService, metricsService *service.MetricsService) *Server {
 	app := fiber.New()
+	app.Use(cors.New())
 	server := &Server{
 		App:            app,
 		MetricsService: metricsService,
